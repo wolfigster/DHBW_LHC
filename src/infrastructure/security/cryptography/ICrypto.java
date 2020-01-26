334edee6b0023a5ab34aa2fb88a5c2d0d7bc597e
Johannes <Jeis2519@gmail.com>
2020-01-26 19:52:15 +0100

    Added Crypto

---
+++ src/infrastructure/security/cryptography/ICrypto.java
@@ -0,0 +1,12 @@
+package infrastructure.security.cryptography;
+
+public interface ICrypto {
+
+    static String encrypt(String string) {
+        return null;
+    }
+
+    static String decrypt(String string) {
+        return null;
+    }
+}
