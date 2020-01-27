482c0bf550c085415ae086fa39fd58f41c57cb0e
Johannes <Jeis2519@gmail.com>
2020-01-27 21:10:57 +0100

    Added Helper

---
+++ src/main/Helper.java
@@ -0,0 +1,52 @@
+package main;
+
+import java.util.Random;
+
+public class Helper {
+
+    public static boolean withInput;
+
+    public static String getAlphaNumericString(int n) {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+        StringBuilder stringBuilder = new StringBuilder(n);
+        for(int i = 0; i < n; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            stringBuilder.append(AlphaNumericString.charAt(index));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String getNumericString(int n) {
+        String AlphaNumericString = "0123456789";
+        StringBuilder stringBuilder = new StringBuilder(n);
+        for(int i = 0; i < n; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            stringBuilder.append(AlphaNumericString.charAt(index));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String getCharString(int n) {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        + "abcdefghijklmnopqrstuvxyz";
+        StringBuilder stringBuilder = new StringBuilder(n);
+        for(int i = 0; i < n; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            stringBuilder.append(AlphaNumericString.charAt(index));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static int[][] createTwoDimensionalIntegerArray(int row, int col) {
+        Random random = new Random();
+        int[][] iris = new int[row][col];
+        for(int i = 0; i < col; i++) {
+            for(int j = 0; j < row; j++) {
+                iris[i][j] = random.nextInt(10);
+            }
+        }
+        return iris;
+    }
+}
