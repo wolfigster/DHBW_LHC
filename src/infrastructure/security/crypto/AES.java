d29ecb949410e51a714a6ba723e0fdfcc3ef6597
Johannes <Jeis2519@gmail.com>
2020-01-27 15:07:09 +0100

    Added AES with Interface and KeyStore

---
+++ src/infrastructure/security/crypto/AES.java
@@ -0,0 +1,30 @@
+package infrastructure.security.crypto;
+
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class AES implements ICrypto {
+
+    public static String encrypt(String string) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, KeyStore.AES.getSecretKeySpec());
+            return Base64.getEncoder().encodeToString(cipher.doFinal(string.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public static String decrypt(String string) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, KeyStore.AES.getSecretKeySpec());
+            return new String(cipher.doFinal(Base64.getDecoder().decode(string)));
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+        return null;
+    }
+}
