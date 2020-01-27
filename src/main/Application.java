6b616fd166de80116365fec15580036555d71844
Johannes <Jeis2519@gmail.com>
2020-01-27 21:10:19 +0100

    Added start parameter to main

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,20 +1,45 @@
 package main;
 
-import human_resources.ReceptionStaff;
-import human_resources.Researcher;
-import human_resources.Visitor;
+import human_resources.*;
 import infrastructure.security.Reception;
 import infrastructure.security.SecurityCenter;
+import infrastructure.security.idcard.EmployeeIDCard;
 import infrastructure.security.idcard.IDCard;
 import infrastructure.security.management.IDCardManagement;
 
 public class Application {
 
-
     public static void main(String... args) {
         init();
-        useCase1();
-        useCase2();
+        for(String arg : args) {
+            if (arg.equals("input")) {
+                Helper.withInput = true;
+                break;
+            }
+        }
+        for(String arg : args) {
+            if(arg.equals("1")) {
+                useCase1();
+            }
+            if(arg.equals("2")) {
+                useCase2();
+            }
+            if(arg.equals("3")) {
+                useCase3();
+            }
+            if(arg.equals("4")) {
+                useCase4();
+            }
+            if(arg.equals("5")) {
+                useCase5();
+            }
+            if(arg.equals("6")) {
+                useCase6();
+            }
+            if(arg.equals("7")) {
+                useCase7();
+            }
+        }
     }
 
     public static void init() {
@@ -26,8 +51,8 @@ public class Application {
         Reception.instance.initialize(receptionStaff01);
 
         //SecurityOfficer securityOfficer01 = new SecurityOfficer("SecurityOfficer01");
-        ReceptionStaff receptionStaff02 = new ReceptionStaff("ReceptionStaff02");
-        SecurityCenter.instance.initialize(receptionStaff02);
+        SecurityStaff securityStaff01 = new SecurityStaff("SecurityStaff01");
+        SecurityCenter.instance.initialize(securityStaff01);
     }
 
     public static void useCase1() {
@@ -40,18 +65,26 @@ public class Application {
     public static void useCase2() {
         System.out.println("Starting useCase2: Creation of an employee ID-Card");
         Researcher researcher01 = new Researcher("Researcher01");
-        IDCard idCard = SecurityCenter.instance.getReceptionStaff().createIDCard(researcher01);
-        SecurityCenter.instance.getReceptionStaff().assignIDCard(idCard, researcher01);
+        EmployeeIDCard employeeIDCard = SecurityCenter.instance.getSecurityStaff().createEmployeeIDCard(researcher01);
+        SecurityCenter.instance.getSecurityStaff().assignEmployeeIDCard(employeeIDCard, researcher01);
     }
 
     public static void useCase3() {
         System.out.println("Starting useCase3: Reader checks access for visitor");
+        Visitor visitor01 = new Visitor("Visitor01");
+        IDCard idCard = Reception.instance.getReceptionStaff().createIDCard(visitor01);
+        Reception.instance.getReceptionStaff().assignIDCard(idCard, visitor01);
+        System.out.println(SecurityCenter.instance.getReader().hasAccess(visitor01)); // false
 
     }
 
     public static void useCase4() {
         System.out.println("Starting useCase4: Reader checks access for employee");
-
+        // useCase2
+        ScientificAssistant scientificAssistant01 = new ScientificAssistant("ScientificAssistant01");
+        EmployeeIDCard employeeIDCard = SecurityCenter.instance.getSecurityStaff().createEmployeeIDCard(scientificAssistant01);
+        SecurityCenter.instance.getSecurityStaff().assignEmployeeIDCard(employeeIDCard, scientificAssistant01);
+        System.out.println(SecurityCenter.instance.getReader().hasAccess(scientificAssistant01)); // true
     }
 
     public static void useCase5() {
@@ -60,12 +93,17 @@ public class Application {
     }
 
     public static void useCase6() {
-        System.out.println("Starting useCase6: HRA has readable access to the employee data");
+        System.out.println("Starting useCase6: HRAssistant has readable access to the employee data");
 
     }
 
     public static void useCase7() {
         System.out.println("Starting useCase7: Security Center locks ID-Card");
+        // useCase2
+        ScientificAssistant scientificAssistant01 = new ScientificAssistant("ScientificAssistant01");
+        EmployeeIDCard employeeIDCard = SecurityCenter.instance.getSecurityStaff().createEmployeeIDCard(scientificAssistant01);
+        SecurityCenter.instance.getSecurityStaff().assignEmployeeIDCard(employeeIDCard, scientificAssistant01);
 
+        SecurityCenter.instance.getSecurityStaff().lockIDCard(scientificAssistant01.getIdCard());
     }
 }
