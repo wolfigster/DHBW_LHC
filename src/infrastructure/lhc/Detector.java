d5fb7fc7b4e3b2554550aebe901597b07007abb1
Johannes <Jeis2519@gmail.com>
2020-01-27 15:17:00 +0100

    Added constructors (energy, lhc)

--- src/infrastructure/lhc/Detector.java
+++ src/infrastructure/lhc/Detector.java
@@ -11,4 +11,22 @@ public class Detector {
     private boolean isActivated;
     private LinkedList<Experiment> experimentList;
     private Reader reader;
+
+    public Detector(boolean isActivated, LinkedList<Experiment> experimentList, Reader reader) {
+        this.isActivated = isActivated;
+        this.experimentList = experimentList;
+        this.reader = reader;
+    }
+
+    public boolean isActivated() {
+        return this.isActivated;
+    }
+
+    public LinkedList<Experiment> getExperimentList() {
+        return this.experimentList;
+    }
+
+    public Reader getReader() {
+        return this.reader;
+    }
 }
