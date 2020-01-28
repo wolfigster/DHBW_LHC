60eceb535b20a05aa5f54ffcce33ec83bf44f676
Johannes | A <Jeis2519@gmail.com>
2020-01-28 15:37:18 +0100

    Added readFunc in IReader and writeFunc in IWriter

--- src/infrastructure/security/device/Reader.java
+++ src/infrastructure/security/device/Reader.java
@@ -1,13 +1,19 @@
 package infrastructure.security.device;
 
 import human_resources.Person;
+import infrastructure.security.idcard.EmployeeIDCard;
 import infrastructure.security.idcard.IDCard;
 import infrastructure.security.idcard.Permission;
+import infrastructure.security.idcard.VisitorIDCard;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Reader implements IReader {
 
     private String currentEmployeeIris;
     private IDCard currentIDCard;
+    private IReaderTechnology iReaderTechnology;
     private Touchpad touchpad;
     private IrisScanner irisScanner;
 
@@ -17,13 +23,62 @@ public class Reader implements IReader {
     }
 
     @Override
-    public void insertIDCard(IDCard idCard) {
-        currentIDCard = idCard;
+    public void insertIDCard(VisitorIDCard visitorIDCard, IReaderTechnology iReaderTechnology) {
+        this.currentIDCard = visitorIDCard;
+        this.iReaderTechnology = iReaderTechnology;
+    }
+
+    @Override
+    public void insertIDCard(EmployeeIDCard employeeIDCard, IReaderTechnology iReaderTechnology) {
+        currentIDCard = employeeIDCard;
     }
 
     @Override
     public void removeIDCard() {
 
+    }
+
+    @Override
+    public String readPasswordFromIDCard() {
+        return currentIDCard.getPasswordChip().read();
+    }
+
+    @Override
+    public String readFingerprintFromIDCard() {
+        EmployeeIDCard employeeIDCard = (EmployeeIDCard) currentIDCard;
+        return employeeIDCard.getFingerChip().read();
+    }
+
+    @Override
+    public int[][] readIrisStructureFromIDCard() {
+        EmployeeIDCard employeeIDCard = (EmployeeIDCard) currentIDCard;
+        return employeeIDCard.getIrisStructure();
+    }
+
+    @Override
+    public boolean readLockedStateFromIDCard() {
+        return currentIDCard.isLocked();
+    }
+
+    @Override
+    public ArrayList<Permission> readPermissionListFromIDCard() {
+        return currentIDCard.getPermissionList();
+    }
+
+    @Override
+    public Date readValidFromIDCard() {
+        return currentIDCard.getValidFrom();
+    }
+
+    @Override
+    public Date readValidUntilIDCard() {
+        return currentIDCard.getValidUntil();
+    }
+
+    @Override
+    public boolean readIsValidDate() {
+        Date now = new Date();
+        return (now.after(readValidFromIDCard()) && now.before(readValidUntilIDCard()));
     }
 
     public Touchpad getTouchpad() {
--- src/infrastructure/security/device/Writer.java
+++ src/infrastructure/security/device/Writer.java
@@ -2,16 +2,40 @@ package infrastructure.security.device;
 
 import infrastructure.security.idcard.EmployeeIDCard;
 import infrastructure.security.idcard.IDCard;
+import infrastructure.security.idcard.Permission;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Writer implements IWriter{
 
     @Override
-    public void writeOnIDCardPWChip(IDCard idCard, String password) {
+    public void writePasswordOnIDCard(IDCard idCard, String password) {
         idCard.getPasswordChip().write(password);
     }
 
     @Override
-    public void writeOnIDCardFNChip(EmployeeIDCard employeeIDCard, String fingerprint) {
+    public void writeFingerprintOnEmployeeCard(EmployeeIDCard employeeIDCard, String fingerprint) {
         employeeIDCard.getFingerChip().write(fingerprint);
+    }
+
+    @Override
+    public void writeIrisStructureOnEmployeeCard(EmployeeIDCard employeeIDCard, int[][] iris) {
+        employeeIDCard.setIrisStructure(iris);
+    }
+
+    @Override
+    public void writeValidFromOnIDCard(IDCard idCard, Date validFrom) {
+        idCard.setValidFrom(validFrom);
+    }
+
+    @Override
+    public void writeValidUntilOnIDCard(IDCard idCard, Date validUntil) {
+        idCard.setValidUntil(validUntil);
+    }
+
+    @Override
+    public void writePermissionListOnIDCard(IDCard idCard, ArrayList<Permission> permissionList) {
+        idCard.setPermissionList(permissionList);
     }
 }
